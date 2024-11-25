90d3ed042dd3c0a55ea4eef2e34512d5b2ac56f1
Lucas Alisson <you@example.com>
2024-11-25 10:41:26 -0300

    Subindo o projeto completo para o repositório

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, World!</h1>
+      <p>Bem-vindo(a) à minha aplicação React!</p>
+    </div>
+  );
+}
+
+export default App;
